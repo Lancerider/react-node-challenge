61b517364e3ebd3661a134092d8780e20c0a8845
Jorge Luis Palacios <jorgepalacioscl@gmail.com>
2022-04-26 13:21:22 -0400

    :recycle: refactoring main react app

--- src/App.js
+++ src/App.js
@@ -1,99 +1,13 @@
-import React, { useState, useEffect } from "react";
-import JobsList from './components/JobsList'
-
-import { getJobs, searchJobs, markFavoriteJob, clearJobs } from './api/jobs'
+import React from "react";
+import SearchView from "./containers/Search";
 
 import "./App.css";
 
 
 function App() {
-  const [searching, setSearching] = useState(false);
-  const [errorSearching, setErrorSearching] = useState(false);
-  const [term, setTerm] = useState('');
-  const [jobs, setJobs] = useState({})
-
-  const getUserJobs = async () => {
-    setSearching(true)
-
-    try {
-      const fetchedJobs = await getJobs()
-      setJobs(fetchedJobs)
-      localStorage.setItem('jobs', fetchedJobs)
-    } catch (error) {
-      console.error(error)
-      const jobsInLocalStorage = localStorage['jobs']
-
-      setJobs(jobsInLocalStorage || [])
-    }
-
-    setSearching(false)
-  }
-
-  const setFavoriteJob = async (jobId) => {
-    try {
-      const responseJobs = await markFavoriteJob(jobId)
-      setJobs(responseJobs)
-    } catch (error) {
-      console.error("Error setting favorite Job: ", error)
-    }
-  }
-
-  const clearSearch = async () => {
-    try {
-      const responseJobs = await clearJobs()
-      setJobs(responseJobs)
-    } catch (error) {
-      console.error("Error erasing user's Jobs: ", error)
-    }
-  }
-
-  const search = async () => {
-    setErrorSearching(false)
-    setSearching(true)
-    const formattedTerms = term.split(' ').join("+")
-    
-    try {
-      const fetchedJobs = await searchJobs(formattedTerms)
-
-      localStorage.setItem('jobs', fetchedJobs)
-      setJobs(fetchedJobs)
-    } catch (error) {
-      console.error("Error searching Jobs: ", error)
-      setErrorSearching(true)
-    }
-    
-    setSearching(false)
-  }
-
-  useEffect(() => {
-    getUserJobs()
-  }, [])
-
   return (
     <div className="App">
-      <h1>Search jobs</h1>
-      <label htmlFor="term">Term: </label>
-
-      <input
-        id="term"
-        type="text"
-        value={term}
-        onChange={({ target: { value } }) => setTerm(value)}
-      />
-
-      <button className="job-list__actions" type="button" onClick={search}>
-        Search
-      </button>
-
-      <button className="job-list__actions" type="button" onClick={clearSearch}>
-        Clear
-      </button>
-
-      { searching && <div>...searching</div> }
-
-      { !searching && !errorSearching && <JobsList jobs={jobs} setFavoriteJob={setFavoriteJob}/> }
-
-      { errorSearching && <div>Ups, there was an error searching for jobs. Please, try again.</div> }
+      <SearchView />
     </div>
   );
 }
